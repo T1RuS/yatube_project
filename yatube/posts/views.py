55ce679f20c0210ca232e0e48baa65e1dec58f27
Михутов Тимур <tumka5688g@gmail.com>
2022-04-12 18:34:53 +0300

    Добавил группы и перебрал верстку.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
     templates = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': "Это главная страница проекта Yatube"
+        'posts': posts
     }
     return render(request, templates, context)
 
 
-def group_posts(request, pk):
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     templates = 'posts/group_list.html'
     context = {
-        'title': "Здесь будет информация о группах проекта Yatube"
+        'group': group,
+        'posts': posts
     }
-    return render(request, templates)
+    return render(request, templates, context)
