247e872f0eb2afc08b42857cdc199b32360316d5
Михутов Тимур <tumka5688g@gmail.com>
2022-04-21 21:06:23 +0300

    Добавил приложение user

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('group/<slug:slug>/', include('posts.urls', namespace='posts')),
     path('user/<slug:slug>/', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
